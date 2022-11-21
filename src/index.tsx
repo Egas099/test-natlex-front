8ea328557923a034371772b06fdef3386af168f7
Egor Dmitriev <egas099@gmail.com>
2022-11-21 17:08:47 +0300

    add prettier config

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./css/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './css/index.css';
 
 const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
